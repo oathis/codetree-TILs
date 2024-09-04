7489ea4d2ee1dabba915c7b95a08e12edba57fcb
oathis <indieedgycoool@gmail.com>
2024-09-04 13:59:10 +0900

    Title: 체질량지수, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240904/체질량지수/bmi.py
+++ 240904/체질량지수/bmi.py
@@ -1,10 +1,12 @@
 import math
 
 height,weight=map(int,input().split())
-if bmi >= 25:
-    c = True
 bmi=(10000*weight)/(height**2)
+C=0
+if bmi >= 25:
+    c = 1
+
 bmi= int(math.floor(bmi*10)/10)
 print(bmi)
-if C:
+if C == 1:
     print("Obesity")
